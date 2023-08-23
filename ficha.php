2c74c7f67c3b21b1ba303b01fdabd65f19a6bf04
Carlos Osmar <carlososmarof@gmail.com>
2023-08-22 21:40:46 -0400

    ficha_2.0

--- ficha.php
+++ ficha.php
@@ -15,7 +15,7 @@
         <table class="table table-dark table-striped-columns">
             <thead class="bg-info">
               <tr>
-                <th scope="col">#</th>
+                <!-- <th scope="col">#</th> -->
                 <th scope="col">Nombre Doctor</th>
                 <th scope="col">Apellido Doctor</th>
                 <th scope="col">Telefono Doctor</th>
@@ -28,13 +28,13 @@
                 include "conexion.php";
                 $medico_id=$_POST['medico_id'];
                 $fecha=$_POST['fecha'];
-                $fecha = $_SESSION['id'];
-                $sql = "SELECT m.nombre as 'Nombre_Medico', m.apellido as 'Apellido_Medico', m.telefono as 'Telefono_Medico', c.fechacita as 'Fecha' FROM medico m INNER JOIN citas c ON c.medico_id = m.id INNER JOIN paciente p ON c.paciente_id = p.id WHERE c.medico_id = m.id and c.paciente_id = p.id";
+                $paciente_id = $_SESSION['id'];
+                $sql = "SELECT m.nombre as 'Nombre_Medico', m.apellido as 'Apellido_Medico', m.telefono as 'Telefono_Medico', c.fechacita as 'Fecha' FROM medico m 
+                INNER JOIN citas c ON c.medico_id = m.id INNER JOIN paciente p ON c.paciente_id = '$paciente_id' WHERE c.medico_id = m.id and c.paciente_id = '$paciente_id'";
                 $resultado = $con->query($sql);
                 if ($resultado->num_rows > 0) {
                     while($datos = $resultado -> fetch_assoc()) { ?>
                         <tr>
-                            <td><?= $datos['id']?></td>
                             <td><?= $datos['Nombre_Medico'] ?></td>
                             <td><?= $datos['Apellido_Medico'] ?></td>
                             <td><?= $datos['Telefono_Medico'] ?></td>
@@ -53,7 +53,7 @@
         </div>
         <button type="button" class="btn btn-primary btn-lg">Aceptar</button>
         <button type="button" class="btn btn-secondary btn-lg">Atras</button>
-        <meta http-equiv="refresh" content="3; url=read.php" />
+        <!-- <meta http-equiv="refresh" content="3; url=read.php" /> -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
      </body>
 </html> 
