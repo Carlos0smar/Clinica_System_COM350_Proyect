7390b56ba9fd40444963fe8fa2a69152321068c2
JoseMamani <142547601+JoseMamaniS@users.noreply.github.com>
2023-09-03 23:38:45 -0400

    Update ficha.php

--- ficha.php
+++ ficha.php
@@ -5,21 +5,21 @@
         <meta charset="UTF-8" >
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/ficha.css">
         <title>Document</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     </head>
     <body>
-        <h1 class =" p-3">Citas Medicas</h1>
-    <div class="col-8 p-4">
-        <table class="table table-dark table-striped-columns">
-            <thead class="bg-info">
+    <div class="table-container">
+    <h1 class =" heading">Citas Medicas</h1>
+        <table class="table">
+            <thead>
               <tr>
-                <!-- <th scope="col">#</th> -->
                 <th scope="col">Nombre Doctor</th>
                 <th scope="col">Apellido Doctor</th>
                 <th scope="col">Telefono Doctor</th>
+                <th scope="col">fecha</th>
                 <th scope="col">Hora</th>
-                <!-- <th scope="col"></th> -->
+                <th scope="col">Acciones</th>
               </tr>
             </thead>
             <tbody>
@@ -30,8 +30,8 @@
                 // $medico_id=$_POST['medico_id'];
                 // $fecha=$_POST['fecha'];
                 $paciente_id = $_SESSION['id'];
-                $sql = "SELECT m.nombre as 'Nombre_Medico', m.apellido as 'Apellido_Medico', m.telefono as 'Telefono_Medico', c.fechacita as 'Fecha' FROM medico m 
-                INNER JOIN citas c ON c.medico_id = m.id INNER JOIN paciente p ON c.paciente_id = p.id WHERE c.medico_id = m.id and c.paciente_id = '$paciente_id'";
+                $sql = "SELECT m.nombre as 'Nombre_Medico', m.apellido as 'Apellido_Medico', m.telefono as 'Telefono_Medico',c.fecha_mes as 'Fecha', c.fechaCita as 'Hora' FROM medico m 
+                INNER JOIN citas c ON c.medico_id = m.id INNER JOIN paciente p ON c.paciente_id = '$paciente_id' WHERE p.id = '$paciente_id'";
                 $resultado = $con->query($sql);
                 if ($resultado->num_rows > 0) {
                     while($datos = $resultado -> fetch_assoc()) { ?>
@@ -40,7 +40,8 @@
                             <td><?= $datos['Apellido_Medico'] ?></td>
                             <td><?= $datos['Telefono_Medico'] ?></td>
                             <td><?= $datos['Fecha'] ?></td>
-                            <!-- <td><button type="button" class="btn btn-primary">Editar</button></td>  -->
+                            <td><?= $datos['Hora'] ?></td>
+                            <td><button type="button" class="ver">Ver</button></td>
                         </tr>
                     <?php }     
                 }?>
@@ -53,8 +54,7 @@
           </table>
         </div>
         <!-- <button onclick="inicio()"type="button" class="btn btn-primary btn-lg">Aceptar</button> -->
-        <a href="javascript: redirecIndex()"><button class="btn btn-primary btn-lg">Aceptar</button></a>
+        <a href="javascript: redirecIndex()"><button class="aceptar">Aceptar</button></a>
         <!-- <meta http-equiv="refresh" content="3; url=read.php" /> -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
      </body>
 </html> 
