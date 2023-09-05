21b389f8231d665edef20ca02b4eea71cbad3367
Marco Antonio Laime Gonzales <49267011+marco-laime@users.noreply.github.com>
2023-09-05 17:07:41 -0400

    Update listar.php

--- listar.php
+++ listar.php
@@ -18,10 +18,15 @@ $sql = "SELECT id, nombre, apellido, genero, direccion, telefono FROM paciente W
 $resultado = $con->query($sql);
 
 if ($resultado->num_rows > 0) {
-    echo '<form action="javascript: registrarHistoria()"  id="formHistoria" class="main-form">';
+    //echo '<form action="javascript: registrarHistoria()"  id="formHistoria" class="main-form">';
+    echo '<form action="javascript: registrarHistoria()"  id="formHistoria" class="">';
+
     while ($row = $resultado->fetch_assoc()) {
+        echo'<div class="column">';
         echo '<label class="form-label">Nombre:</label><br>';
         echo '<input type="text" class="form-field" name="nombre" value="' . $row['nombre'] . '" readonly><br>';
+        
+
         
         echo '<label class="form-label">Apellido:</label><br>';
         echo '<input type="text" class="form-field" name="apellido" value="' . $row['apellido'] . '" readonly><br>';
@@ -31,9 +36,14 @@ if ($resultado->num_rows > 0) {
         
         echo '<label class="form-label">Dirección:</label><br>';
         echo '<input type="text" class="form-field" name="direccion" value="' . $row['direccion'] . '" readonly><br>';
-        
+         
         echo '<label class="form-label">Teléfono:</label><br>';
         echo '<input type="text" class="form-field" name="telefono" value="' . $row['telefono'] . '" readonly><br>';
+
+        echo'</div>';
+      
+        echo'<div class="column">';
+
         
         echo '<label class="form-label">Tipo de Sangre:</label><br>';
         echo '<input type="text" class="form-field" name="tipo_sangre" placeholder="Tipo de Sangre"><br>';
@@ -43,16 +53,31 @@ if ($resultado->num_rows > 0) {
         
         echo '<label class="form-label">Estatura:</label><br>';
         echo '<input type="text" class="form-field" name="estatura" placeholder="Estatura"><br>';
+
+        echo '<label class="form-label">Alergia:</label><br>';
+        echo '<input type="text" class="form-field" name="alergia" placeholder="Alegia"><br>';
+
+        echo '<label class="form-label">Fecha de nacimiento:</label><br>';
+        echo '<input type="text" class="form-field" name="fecha_nac" placeholder="Fecha de nacimiento"><br>';
+
+        echo'</div>';
         
-        echo '<label class="form-label">Descripción:</label><br>';
+        
+        echo '<div class="column"><label class="form-label">Descripción:</label></div><br>';
         echo '<textarea class="form-textarea" name="descripcion" placeholder="Descripción" rows="4"></textarea><br>';
 
         echo '<input type="hidden" class="form-field" name="id" value="' . $row['id'] . '" readonly><br>';
+   
         
        
     }
+   
+  
+    
     echo '<input type="submit" class="form-button" value="Enviar">';
     echo '</form>';
+
+    
 } else {
     echo "No se encontraron resultados.";
 }
