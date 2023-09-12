1e5ad293e11e42e2fa54ad800cf7ec62d7b42643
JoseMamani <142547601+JoseMamaniS@users.noreply.github.com>
2023-09-12 02:38:16 -0400

    Update createMedico.php

--- createMedico.php
+++ createMedico.php
@@ -8,19 +8,25 @@ $telefono=$_POST['telefono'];
 $informacion=$_POST['informacion'];
 $especialidad=$_POST['especialidad'];
 $email=$_POST['email'];
+$sala =$_POST['num_consultorio'];
+$estadoSala ='No Disponible';
+$id=$_POST['id'];
 
 
 $sql="INSERT into medico (nombre, apellido, edad, telefono, informacion,especialidad, email )
 VALUES ('$nombre', '$apellido', '$edad', '$telefono', '$informacion', '$especialidad', '$email')";
+$sql2 ="INSERT INTO sala (num_consultorio,estado,id_medico) VALUES ('$sala', '$estadoSala','$id')";
 
 
-if ($con->query($sql) === TRUE) {
-
+if ($con->query($sql)  === TRUE) {
+    if($con->query($sql2) === TRUE) {
     $response = array('redirect' => 'readMedico.php');
     echo json_encode($response);
+    }
 } else {
     echo "Error: " . $sql . "<br>" . $con->error;
 }
+
 
 ?>
 
